64ece750b4a0ea4219089eda41348e133537a81f
Brunston Poon <albatrossian@outlook.com>
2017-04-07 22:06:15 -0700

    first pass alternate_detect

--- alt_detect.py
+++ alt_detect.py
@@ -5,12 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import time
-#import picamera
+import picamera
+import os
+# contributors: Brunston Poon, Dinesh Parimi
 
 # variables and things that don't change
 START_TIME = int(round(time.time() * 1000.0)) # in ms
 BTWN_PHOTOS = 1000 # ms
 PCNT_PHOTO_DETECTED = 14400 # 1600x900 * 1% of the image
+ONE = np.array([[[0, 0.1255, 0.3569]]], dtype='float32')
+TWO = np.array([[[ 1, 0.8196, 0]]], dtype='float32')
+THREE = np.array([[[0.6510, 0.0353, 0.2392]]], dtype='float32')
+
 current_photo_num = 1
 
 # detection on image 
@@ -25,27 +31,30 @@ def detect(image, rgb):
     im_unit -= 0.05 # 5 percent variance
     whr = np.where(im_unit<0)
     plt.imshow(im_unit)
-    plt.imshow("detected1")
     if (len(whr[0]<0) > PCNT_PHOTO_DETECTED or len(whr[1]<0) > PCNT_PHOTO_DETECTED or len(whr[2]<0) > PCNT_PHOTO_DETECTED):
-        print(whr[0])
-        print(im_unit[0])
         for i in range(len(whr[0])):
-            im_unit[whr[0]][i] = 255
-            im_unit[1][i] = 255
-            im_unit[2][i] = 255
+            im_unit[whr[0][i]][whr[1][i]][0] = 255
+            im_unit[whr[0][i]][whr[1][i]][1] = 255
+            im_unit[whr[0][i]][whr[1][i]][2] = 255
         imsave("detected" + str(current_photo_num), im_unit, format="png")
+        os.remove("photosmall" + str(current_photo_num))
+    else:
+        os.remove("photo" + str(current_photo_num))
+    
     plt.show()
 
 def capture_store_detect_label():
-#    camera = PiCamera(resolution=(1600,900))
-#    camera.capture("photo" + str(current_photo_num))
+    global current_photo_num
+    camera = PiCamera(resolution=(1600,900))
+    camera.capture("photo" + str(current_photo_num))
+    camera.capture("photosmall" + str(current_photo_num), resize=(400,225))
+    detect("photo" + str(current_photo_num), ONE)
+    detect("photo" + str(current_photo_num), TWO)
+    detect("photo" + str(current_photo_num), THREE)
     current_photo_num += 1
-    # red
-    # blue
-    # green
 
 # test
-detect('minor.jpg', np.array([[[0.9373, 0.9373, 0.9373]]], dtype='float32'))
+#detect('minor.jpg', np.array([[[0.9373, 0.9373, 0.9373]]], dtype='float32'))
 
 # loop
 while True:
